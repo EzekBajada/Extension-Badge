987c5e3e560a4ce16f0f978827f09bddaeb204a2
EzuBotz <ezek_bajada@hotmail.co.uk>
2018-11-10 17:37:57 +0100

    Added progress bar and the request for limits

--- popup.js
+++ popup.js
@@ -3,19 +3,13 @@
 // found in the LICENSE file.
 
 'use strict';
-
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+document.addEventListener("DOMContentLoaded", function(){
+  const Http = new XMLHttpRequest();
+  const url = "http://127.0.0.1:5000/getData";
+  Http.open("GET", url);
+  Http.send();
   
+  Http.onreadystatechange=(e)=>{
+    console.log(Http.responseText)
+}
+});
